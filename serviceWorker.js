b061df04a4cb4716e950b8b2ad2b2b9c271e6542
anshu190 <anshurajhans563@gmail.com>
2025-11-17 17:42:37 +0530

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,23 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
+const CACHE_NAME = "dice-v1";
+const urlsToCache = [
+  "/K/",
+  "/K/index.html",
+  "/K/manifest.json",
+  "/K/icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
